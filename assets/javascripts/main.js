30d92fd5dcffe613c4ce7e4d6ee62bc8882a8fed
LeaG76 <leagallier.lag@gmail.com>
2023-03-05 21:38:18 +0100

    Added scroll to the top button

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -48,3 +48,16 @@ function scrollActive(){
 }
 
 window.addEventListener('scroll', scrollActive);
+
+/* Show scroll top */
+
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    if(this.scrollY >= 200) {
+        scrollTop.classList.add('show-scroll');
+    } else {
+        scrollTop.classList.remove('show-scroll');
+    }
+}
+
+window.addEventListener('scroll', scrollTop);
