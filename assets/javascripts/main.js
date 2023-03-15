063a65edef205d01aca20828c32f41e6fbf9ffd7
LeaG76 <leagallier.lag@gmail.com>
2023-03-15 13:42:27 +0100

    PDF Download with html2pdf.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -77,3 +77,47 @@ themeButton.addEventListener('click', () => {
         themeButton.classList.remove('fa-sun');
     }
 });
+
+/* Reduce the size and print on an A4 sheet */
+
+function addScaleCV() {
+    document.body.classList.add("scale-cv");
+}
+
+/* Remote the size when the CV is downloaded */
+
+function removeScaleCV() {
+    document.body.classList.remove("scale-cv");
+}
+
+/* Generate PDF */
+
+// PDF generated area
+let areaCV = document.getElementById('area-cv');
+
+// Button
+let resumeButton = document.getElementById("resume-button");
+
+// html2pdf.js options
+let opt = {
+    margin:       0,
+    filename:     'myResume.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 4, useCORS: true },
+    jsPDF:        { format: 'a4', orientation: 'portrait' }
+  };
+
+// Generate PDF with html2pdf.js
+function generateResume() {
+    html2pdf(areaCV, opt);
+}
+
+// Action executed by clicking on the button => generation of the final PDF CV CV
+resumeButton.addEventListener("click", () => {
+    // Adapt the area of the PDF
+    addScaleCV();
+    // Generate the PDF
+    generateResume();
+    // Remove adaptation after 1 second (you can choose to set more than 1 second if your PDF download time is long)
+    setTimeout(removeScaleCV, 1000);
+});
